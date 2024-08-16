a330cd4fb1579765928432acb6a5ba0b2e63a508
localhostov <localhostov@gmail.com>
2024-08-16 23:30:17 +0300

    feat: add more colors

--- uikit/src/main/java/psbmarket/uikit/theme/ColorScheme.kt
+++ uikit/src/main/java/psbmarket/uikit/theme/ColorScheme.kt
@@ -5,9 +5,19 @@ import androidx.compose.ui.graphics.Color
 public data class ColorScheme(
     val accent: Color,
     val text: Color,
+    val background: Color,
+    val bottomNavigationBackground: Color,
+    val bottomNavigationDot: Color,
+    val bottomNavigationIconActive: Color,
+    val bottomNavigationIconInactive: Color,
 )
 
 public val defaultColorScheme: ColorScheme = ColorScheme(
     accent = Color(0xFF4748D9),
-    text = Color(0xFFFFFFFF)
+    text = Color(0xFFFFFFFF),
+    background = Color(0xFF000000),
+    bottomNavigationBackground = Color(0xFF141414),
+    bottomNavigationDot = Color(0xFF4D4D4D),
+    bottomNavigationIconActive = Color(0xFFFFFFFF),
+    bottomNavigationIconInactive = Color(0xFF595959)
 )
