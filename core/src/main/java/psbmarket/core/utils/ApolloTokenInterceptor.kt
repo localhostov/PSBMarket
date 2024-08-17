17b04d9c297fc7e10b6f882e6bd19911456dc627
localhostov <localhostov@gmail.com>
2024-08-18 00:14:28 +0300

    chore: get token from storage

--- core/src/main/java/psbmarket/core/utils/ApolloTokenInterceptor.kt
+++ core/src/main/java/psbmarket/core/utils/ApolloTokenInterceptor.kt
@@ -1,23 +1,32 @@
 package psbmarket.core.utils
 
+import android.content.Context
 import com.apollographql.apollo.api.ApolloRequest
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.interceptor.ApolloInterceptor
 import com.apollographql.apollo.interceptor.ApolloInterceptorChain
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import psbmarket.core.Constants
+import psbmarket.core.Preferences
+import psbmarket.core.extensions.dataStore
 
-internal class ApolloTokenInterceptor : ApolloInterceptor {
+internal class ApolloTokenInterceptor(
+    private val context: Context
+) : ApolloInterceptor {
     override fun <D : Operation.Data> intercept(
         request: ApolloRequest<D>,
         chain: ApolloInterceptorChain
-    ): Flow<ApolloResponse<D>> {
+    ): Flow<ApolloResponse<D>> = flow {
+        val accessToken = context.dataStore.data.first()[Preferences.accessToken] ?: Constants.BASIC_API_TOKEN
         val modifiedRequest = request
             .newBuilder()
-            .addHttpHeader("Authorization", Constants.BASIC_API_TOKEN)
+            .addHttpHeader("Authorization", accessToken)
             .build()
 
-        return chain.proceed(modifiedRequest)
+        emitAll(chain.proceed(modifiedRequest))
     }
 }
