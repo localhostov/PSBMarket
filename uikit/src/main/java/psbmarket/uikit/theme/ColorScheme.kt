862aca0eeef5adfa86e850da2d4e499684fccca2
localhostov <localhostov@gmail.com>
2024-08-18 00:16:12 +0300

    feat: add more colors

--- uikit/src/main/java/psbmarket/uikit/theme/ColorScheme.kt
+++ uikit/src/main/java/psbmarket/uikit/theme/ColorScheme.kt
@@ -4,20 +4,64 @@ import androidx.compose.ui.graphics.Color
 
 public data class ColorScheme(
     val accent: Color,
-    val text: Color,
     val background: Color,
+
+    val success: Color,
+    val error: Color,
+
+    val text: Color,
+    val textSecondary: Color,
+    val divider: Color,
+
     val bottomNavigationBackground: Color,
     val bottomNavigationDot: Color,
     val bottomNavigationIconActive: Color,
     val bottomNavigationIconInactive: Color,
+
+    val buttonPrimaryBackground: Color,
+    val buttonPrimaryForeground: Color,
+
+    val buttonSecondaryBackground: Color,
+    val buttonSecondaryForeground: Color,
+
+    val card: Color,
+    val cardSecondary: Color,
+
+    val inputBackground: Color,
+    val inputPlaceholder: Color,
+
+    val toasterBackground: Color,
+    val toasterBorder: Color,
 )
 
 public val defaultColorScheme: ColorScheme = ColorScheme(
     accent = Color(0xFF4748D9),
-    text = Color(0xFFFFFFFF),
     background = Color(0xFF000000),
+
+    success = Color(0xFF41AC0F),
+    error = Color(0xFFAC0F0F),
+
+    text = Color(0xFFFFFFFF),
+    textSecondary = Color(0xFF606060),
+    divider = Color(0xFF2B2B2B),
+
     bottomNavigationBackground = Color(0xFF141414),
     bottomNavigationDot = Color(0xFF4D4D4D),
     bottomNavigationIconActive = Color(0xFFFFFFFF),
-    bottomNavigationIconInactive = Color(0xFF595959)
+    bottomNavigationIconInactive = Color(0xFF595959),
+
+    buttonPrimaryBackground = Color(0xFF2B2B2B),
+    buttonPrimaryForeground = Color.White,
+
+    buttonSecondaryBackground = Color(0xFFFFFFFF),
+    buttonSecondaryForeground = Color.Black,
+
+    card = Color(0xFF1C1C1C),
+    cardSecondary = Color(0xFF333333),
+
+    inputBackground = Color(0xFF121212),
+    inputPlaceholder = Color(0xFF5C5C5C),
+
+    toasterBackground = Color(0xFF262626),
+    toasterBorder = Color(0xFF333333)
 )
--- uikit/src/main/java/psbmarket/uikit/theme/Theme.kt
+++ uikit/src/main/java/psbmarket/uikit/theme/Theme.kt
@@ -1,9 +1,12 @@
 package psbmarket.uikit.theme
 
 import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 
 public object Theme {
@@ -17,6 +20,18 @@ public object Theme {
         @ReadOnlyComposable
         get() = LocalShapes.current
 }
+
+public val defaultTextFieldColors: TextFieldColors
+    @Composable
+    get() = TextFieldDefaults.colors(
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        cursorColor = Theme.colors.accent,
+        unfocusedContainerColor = Theme.colors.inputBackground,
+        focusedContainerColor = Theme.colors.inputBackground,
+        focusedPlaceholderColor = Theme.colors.inputPlaceholder,
+        unfocusedPlaceholderColor = Theme.colors.inputPlaceholder,
+    )
 
 @Composable
 public fun AppTheme(content: @Composable () -> Unit) {
