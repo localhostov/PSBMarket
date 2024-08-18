fa9ce4c2f91af50478cf95a3e236bf9e16b684aa
localhostov <localhostov@gmail.com>
2024-08-19 02:06:29 +0300

    feat: add coil image loader factory

--- app/src/main/java/me/localx/psbmarket/App.kt
+++ app/src/main/java/me/localx/psbmarket/App.kt
@@ -1,7 +1,33 @@
 package me.localx.psbmarket
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("images"))
+                    .maxSizePercent(0.13)
+                    .build()
+            }
+            .networkCachePolicy(CachePolicy.READ_ONLY)
+            .respectCacheHeaders(false)
+            .logger(DebugLogger())
+            .crossfade(true)
+            .build()
+    }
+}
